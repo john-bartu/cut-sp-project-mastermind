c71d62aafa89da2dd6e8c00e7913dc5f09c14aaf
Jamabaiz <bartula.jan@gmail.com>
2021-06-05 13:35:36 +0200

    Remove Polish letters

--- game/Wiadomosci.py
+++ game/Wiadomosci.py
@@ -1,6 +1,6 @@
 
 class Wiadomosci:
-    MSG_END_GAME_LOSE = "Przegrana. Przekroczono liczbę kroków."
-    MSG_END_GAME_WIN = "Wygrana. Kod został odgadnięty."
-    MSG_CHECK_CHEAT_CORRECT = "Zgadza się. Komputer oszukiwał."
-    MSG_CHECK_CHEAT_WRONG = "Tere Fere. Mylisz się."
+    MSG_END_GAME_LOSE = "Przegrana. Przekroczono liczbę krokow."
+    MSG_END_GAME_WIN = "Wygrana. Kod zostal odgadniety."
+    MSG_CHECK_CHEAT_CORRECT = "Zgadza sie. Komputer oszukiwal."
+    MSG_CHECK_CHEAT_WRONG = "Tere Fere. Mylisz sie."
